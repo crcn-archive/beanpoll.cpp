a5575afa1cd0daaaecb8b4533297b3e0c0101bbe
Craig Condon <craig@spiceapps.com>
2011-10-29 18:12:26 -0500

    removed comments

--- src/threading/thread_pool.h
+++ src/threading/thread_pool.h
@@ -22,18 +22,8 @@ namespace Beanpole
 		maxThreads(maxThreads), 
 		minThreads(minThreads),
 		_waitingThreads(0)
-		{                                   
-			// pthread_cond_init(&this->_hasTask, NULL);
-			pthread_mutex_init(&this->_pthreadMutex, NULL);       
-			
-			struct sched_param param;
-			                                            
-			// pthread_attr_init(&this->_tattr);
-			// std::cout <<pthread_attr_setschedpolicy(&this->_tattr, SCHED_FIFO);  
-			// pthread_attr_setinheritsched(&this->_tattr, PTHREAD_EXPLICIT_SCHED);       
-			// param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-			                                                  
-			// pthread_attr_setschedparam(&this->_tattr, &param);
+		{                                                
+			pthread_mutex_init(&this->_pthreadMutex, NULL);         
 		}; 
 		
 		ThreadTask* createTask(void* data,  ThreadCallback* callback );   
@@ -47,8 +37,7 @@ namespace Beanpole
 		std::list<Thread*> _threads;            
 		std::list<Thread*> _waitingThreads; 
 		std::vector<ThreadTask*> _waitingTasks;
-		pthread_mutex_t _pthreadMutex;  
-		// pthread_attr_t _tattr;                      
+		pthread_mutex_t _pthreadMutex;                   
 		void removeThread(Thread*);
 		
 		void run(ThreadTask*);   
