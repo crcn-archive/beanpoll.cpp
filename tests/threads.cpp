44f39a028ebec71be3d1d76a6a8fe7f1af1349d4
Craig Condon <craig@spiceapps.com>
2011-10-30 15:50:17 -0500

    variable usleep

--- tests/threads.cpp
+++ tests/threads.cpp
@@ -1,15 +1,17 @@
 #include "beanpole.cpp"   
 #include <pthread.h>           
 
-int steps = 100 * 100 * 100 ;    
+int steps;    
 pthread_t thread; 
 pthread_mutex_t mutex;
 pthread_cond_t cond;
                                                    
 void* sayHelloWorld(void* request)
 {                                   
-	// std::cout << (const char*)request << " " << --steps << std::endl;     
-	usleep(300 * 1000);
+	std::cout << (const char*)request << " " << steps++ << std::endl;  
+	
+	//simulate busy thread, creating more threads - also variable.   
+	usleep((rand() % 200 + 100)  * 1000);
 	
 	 
 }     
@@ -17,11 +19,7 @@ void* sayHelloWorld(void* request)
 
 int main()
 {                                   
-	                                  
-	// pthread_cond_init(&cond, NULL);		            
-	// pthread_mutex_init(&mutex, NULL);                                      
-	// pthread_create(&thread, NULL, &sayHelloWorld, (void*)"hello world!");
-	// pthread_create(&thread, NULL, &sayHelloWorld, (void*)"hello world!");
+	                                                                            
 	
 	Beanpole::ThreadPool* pool = new Beanpole::ThreadPool(100, 2);  
 	
@@ -29,30 +27,23 @@ int main()
 	
 	double inc = 0;
 	                                
-	                
-	// for(int i = steps; i--;)
+	                               
 	while(1)    
-	{                                   
+	{                                                                          
+		
+		//run the new thread
 		pool->createTask((void*)"hello world", &sayHelloWorld);  
-		
-		// std::cout << sin(inc) * 500 << std::endl;  
-		                                               
-		
-		//simulate lots of activity, then little activity.    
+		                                                
+		                                                     
+		//simulate variable activity. Testing creation & destruction of threads   
 		int timeout = round(abs(sin(inc) * 50)) * 1000; 
-		                                         
+		                                                      
 		
 		inc += 0.01;                    
-		
-		std::cout << "timeout:" << (timeout/1000) << std::endl;
-		                                          
+		                                                                
 		usleep(timeout);
 	}    
 	                                                   
-            
-	int i;
-	
-	std::cin >> i;
-	
+                      
 	                                                            
 }
