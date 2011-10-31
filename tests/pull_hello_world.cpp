20b18dd535d96bdb8e70261d3b8a2dc5eca23f4d
Craig Condon <craig@spiceapps.com>
2011-10-31 13:09:01 -0500

    added middleware test

--- tests/pull_hello_world.cpp
+++ tests/pull_hello_world.cpp
@@ -9,7 +9,20 @@ void onSayHello(Beanpole::PullRequest* request)
 	printf("request: %s\n", (const char*)request->read()); 
 	                                                                  
 	request->end((void*)"hello world!");     
-}   
+}          
+
+void onAuth(Beanpole::PullRequest* request)
+{
+	std::cout << "authenticating..." << std::endl;                
+	                 
+	//hit the database
+	usleep(200*1000);
+	
+	if(request->next())
+	{
+		std::cout << "nexted!" << std::endl;
+	}
+}
 
 
 
@@ -21,12 +34,16 @@ void sayHello(Beanpole::RequestStream* stream)
 int main()
 {       
 	Beanpole::Router* router = new Beanpole::Router();
-	                                 
-	router->on("pull hello/world", &onSayHello);    
+	
+	router->on("pull auth", &onAuth);                                 
+	router->on("pull auth -> hello/:name", &onSayHello);    
 	                                     
                    
-	for(int i = 100; i--;)     
-	router->request("hello/world")->pull((void*)"hello world", &sayHello);  
+	for(int i = 100; i--;) 
+	{    
+		router->request("hello/craig")->pull((void*)"hello world", &sayHello);  
+		// usleep(25*1000);
+	}
 	
 	int i = 0;
 	
