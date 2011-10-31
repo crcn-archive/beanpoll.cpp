9c461d65a74de29af91b54f39aaa85386321ef82
Craig Condon <craig@spiceapps.com>
2011-10-31 16:22:19 -0500

    added plugin example

---
+++ tests/plugin.cpp
@@ -0,0 +1,31 @@
+#include "beanpole.cpp"   
+#include <sched.h>  
+
+             
+void onHelloTest(Beanpole::PushRequest* request)
+{
+	std::cout << "HELLO!" << std::endl;
+}
+  
+                                
+void testPlugin(Beanpole::Router* router)
+{                                       
+	router->on("push hello/test", &onHelloTest);
+}
+
+int main()
+{       
+	Beanpole::Router* router = new Beanpole::Router();                                                                
+	Beanpole::plugin(&testPlugin);      
+	router->on(Beanpole::beans);   
+	
+	
+	router->request("hello/test")->push((void*)"hello/test");
+	      
+	
+	int i;
+	
+	std::cin >> i;
+	           
+	                                                            
+}
